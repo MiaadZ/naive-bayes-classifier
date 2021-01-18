f346156b821ea0bcdb8abccd5a5d3294d198ff69
SeZaR <mdze171@gmail.com>
2021-01-18 22:04:31 +0330

    fixing python script and updating requirements

--- Naive_Bayes_Classifier.py
+++ Naive_Bayes_Classifier.py
@@ -3,26 +3,25 @@ import numpy as np
 from scipy.stats import norm
 import matplotlib.pyplot as plt
 
-# Read Excel File from the input
+"""
+Preprocessing Data
+"""
 data = pd.read_excel('dataset.xlsx')
-
-# Making a For Loop Based on the number of columns and rows
 length = np.shape(data)
 for_length_columns = length[1]
-
-# Creating Empty DataFrame for later use
 w0_column = pd.DataFrame()
 w1_column = pd.DataFrame()
+w0_normal_pdf = 1
+w1_normal_pdf = 1
 
 # Calculating P(w0) and P(w1)
 pw0 = len(data.query('W == 0')) / len(data)
 pw1 = len(data.query('W == 1')) / len(data)
 
-w0_normal_pdf = 1
-w1_normal_pdf = 1
-
-# For Loop considering length of columns without the
-# last column which contains the class labels
+"""
+For loop considering length of columns without the
+last column which contains the class labels
+"""
 for i in range(for_length_columns - 1):
 
     # Choosing a column in each rotation with the labels column
@@ -35,26 +34,25 @@ for i in range(for_length_columns - 1):
     # Fetching Firt Column
     w0_column = w0_column.iloc[:, [0]]
     w1_column = w1_column.iloc[:, [0]]
-    
+
     # Calculating the Average of the Feature records
     # axis=0 for columns and axis=1 for rows
     w0_column_average = w0_column.mean(axis=0)
     w1_column_average = w1_column.mean(axis=0)
-    
+
     # Calculating the Sigma of the Feature records
     w0_column_sigma = w0_column.std(axis=0)
     w1_column_sigma = w1_column.std(axis=0)
 
-    test = [80, 11]
-    #Calculating the normal probabilistic density function
+    # Calculating the normal probabilistic density function considering test data
+    test = [80, 11] # 80: Student IQ, 11: Student Mark
     w0_normal_pdf *= norm.pdf(test[i], w0_column_average, w0_column_sigma)
     w1_normal_pdf *= norm.pdf(test[i], w1_column_average, w1_column_sigma)
 
-# Multiplying the answer with P(w0) and P(w1)
+# Multiplying the answer with P(w0) and P(w1) and showing the output
 w0_normal_pdf = np.multiply(pw0, w0_normal_pdf)
 w1_normal_pdf = np.multiply(pw1, w1_normal_pdf)
 
-# Output
 if w1_normal_pdf > w0_normal_pdf:
     print("Naive Bayes Prediction Passed")
 else:
